fe4323afc83aba37dbbfb864e9afebcab2d44877
ValGuibs <62211010+ValGuibs@users.noreply.github.com>
2025-03-18 12:36:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://29055e2f-4a12-4712-b418-2405caa6e396.weweb-preview.io/", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
+          pattern: "/articles/[^/]+",
+          metaDataEndpoint: "https://api.fccr.fr/api/site-web/articles/metadata/slug/{article_slug}"
       },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
-      }
-      // Add more patterns and their metadata endpoints as needed
   ]
 };
